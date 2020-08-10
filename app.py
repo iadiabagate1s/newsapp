c98ae8e4422d23240eca4dfa4ec346b0d18f3c37
Isaac Diabagate <iadiabagate1s@semo.edu>
2020-08-10 08:42:56 -0400

    need keys

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ from models import db, connect_db, User,Keywords,Likes
 from forms import LoginForm, Register, Edituserform
 from secrets import openweather_api, maps_places_api, news_api_key
 from flask_bcrypt import Bcrypt
+import os
 
 
 app =Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cap'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgres:///cap')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY']= 'malachixxl'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'malachixxl')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
 debug = DebugToolbarExtension(app)
 
---
+++ secrets.py
@@ -0,0 +1,6 @@
+
+openweather_api = '7b70bf3314ed64799900c0b315a53df2'
+
+maps_places_api ='eotpicYMnWHOwiNYfxZAfi8vMOizbSdE'
+
+news_api_key = '567ed6fb99a84bdd934e0910adb37ebd'
